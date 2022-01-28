9355bf7b4881780f46b3bdc7268059f01b65a82d
Reid Messinger <reid.messinger@gmail.com>
2022-01-28 11:59:35 -0500

    Add Combinations template

--- TextTools/TextTools/WordGenerator.cs
+++ TextTools/TextTools/WordGenerator.cs
@@ -87,6 +87,31 @@ namespace TextTools
             }
         }
 
+        private IEnumerable<int[]> Combinations(int m, int n)
+        {
+            int[] result = new int[m];
+            Stack<int> stack = new Stack<int>();
+            stack.Push(0);
+
+            while (stack.Count > 0)
+            {
+                int index = stack.Count - 1;
+                int value = stack.Pop();
+
+                while (value < n)
+                {
+                    result[index++] = ++value;
+                    stack.Push(value);
+
+                    if (index == m)
+                    {
+                        yield return result;
+                        break;
+                    }
+                }
+            }
+        }
+
         private void GenerateWords(char firstLetter)
         {
             WordDictionary wordDictionary = new WordDictionary();
